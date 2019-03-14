603253440416ad6c8f832655ec29ab6045f1637b
subroh0508 <subroh.0508@gmail.com>
2019-03-15 02:11:42 +0900

    [add]Add Android app UI

--- sample/androidapp/src/main/kotlin/net/subroh0508/sparkl/MainActivity.kt
+++ sample/androidapp/src/main/kotlin/net/subroh0508/sparkl/MainActivity.kt
@@ -3,14 +3,28 @@ package net.subroh0508.sparkl
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import net.subroh0508.core.Test
+import net.subroh0508.core.SparqlQuery
+import net.subroh0508.core.Var
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        test.text = Test().hogehoge()
+    override fun onResume() {
+        super.onResume()
+
+        sendRequest.setOnClickListener { sendRequest() }
+    }
+
+    private fun sendRequest() {
+        SparqlQuery("hogehoge").where {
+            triple(Var(""), Var(""), Var(""))
+                +(Var("") to Var(""))
+
+            triple(Var(""), Var(""), Var(""))
+                +(Var("") to Var(""))
+        }
     }
 }
