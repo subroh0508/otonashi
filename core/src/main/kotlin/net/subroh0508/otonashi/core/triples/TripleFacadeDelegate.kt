8b06a9d9e4d392aa64d9fbddb55333a798974a91
subroh0508 <subroh.0508@gmail.com>
2019-04-17 23:34:07 +0900

    [edit]Enable using IRI vocabularies excluded build-up vocabularies

--- core/src/main/kotlin/net/subroh0508/otonashi/core/triples/TripleFacadeDelegate.kt
+++ core/src/main/kotlin/net/subroh0508/otonashi/core/triples/TripleFacadeDelegate.kt
@@ -1,6 +1,7 @@
 package net.subroh0508.otonashi.core.triples
 
 import net.subroh0508.otonashi.core.vocabulary.Vocabulary
+import net.subroh0508.otonashi.triples.Iri
 import net.subroh0508.otonashi.triples.TripleFacade
 import net.subroh0508.otonashi.triples.TripleItem
 import net.subroh0508.otonashi.triples.Var
@@ -17,6 +18,8 @@ internal class TripleFacadeDelegate(
             Var(name)
         else
             vocabulary.v[name.camelize()] ?: Var(name).also { vocabulary.v[name.camelize()] = it }
+
+    override fun iri(name: String) = Iri(name)
 
     override val iri: Set<IriVocabulary> get() = vocabulary.iri
 
--- triples/src/main/kotlin/net/subroh0508/otonashi/triples/TripleFacade.kt
+++ triples/src/main/kotlin/net/subroh0508/otonashi/triples/TripleFacade.kt
@@ -6,6 +6,8 @@ import net.subroh0508.otonashi.triples.vocabulary.IriVocabulary
 interface TripleFacade {
     abstract fun v(name: String, onlyName: Boolean = false): Var
 
+    abstract fun iri(name: String): Iri
+
     val iri: Set<IriVocabulary>
 
     infix fun TripleItem.`as`(v: Var): Var
